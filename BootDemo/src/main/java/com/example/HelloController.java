6862723c9388cf48ff565dc01106e3d36ce44afd
iibi0108 <iibi0108@iibi0108-PC>
2017-06-13 17:17:03 +0900

    HelloController에 test2()메소드 추가

--- BootDemo/src/main/java/com/example/HelloController.java
+++ BootDemo/src/main/java/com/example/HelloController.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class HelloController {
 
-	@RequestMapping("/hi")
+	@RequestMapping("/hello")
 	public String test(Model m){
 		System.out.println("들어옴");
 		return "hello";
+	}
+	@RequestMapping("/hi")
+	public Map<String, String> test2(){
+		Map map=new HashMap();
+		map.put("name","홍길동");
+		map.put("age", "22");
+		return map;
 	}
 }
