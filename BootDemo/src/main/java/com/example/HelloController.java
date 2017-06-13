f13ff36bd495b9d61432ccd6adf4e4c40066df70
iibi0108 <iibi0108@iibi0108-PC>
2017-06-13 17:02:02 +0900

    포트번호를 9999로 수정. HelloController.java내용 수정

--- BootDemo/src/main/java/com/example/HelloController.java
+++ BootDemo/src/main/java/com/example/HelloController.java
@@ -1,17 +1,17 @@
 package com.example;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class HelloController {
 
 	@RequestMapping("/hi")
 	public String test(Model m){
 		System.out.println("들어옴");
-		return "/hello";
+		return "hello";
 	}
 }
